fb5b4208331ab315437957cd8c97c52a34ff0e29
Toby Crawley <toby@tcrawley.org>
2014-09-18 21:39:32 -0400

    Minor refactoring/cleanup.

--- src/immutant/codecs/transit.clj
+++ src/immutant/codecs/transit.clj
@@ -23,28 +23,16 @@
                                                validate-options]])
   (:import [java.io ByteArrayInputStream ByteArrayOutputStream]))
 
-(defn transit-codec
-  "Creates an Immutant codec that can be used to encode to/decode from Transit.
-
-   options can be a map or kwargs, with these valid keys [default]:
-
-   * :name [:transit]
-   * :type [:json]
-   * :content-type [\"application/transit+<:type>\"]
-   * :write-handlers [nil]
-   * :read-handlers [nil]"
-  [& options]
-  (let [{:keys [name content-type type read-handlers write-handlers]
-         :or {name :transit type :json}} (-> options
-                                           kwargs-or-map->raw-map
-                                           (validate-options transit-codec))
-         content-type (or content-type (str "application/transit+"
-                                         (name type)))
-         transit-write (try-resolve-throw `cognitect.transit/write
-                         "add com.cognitect/transit-clj to your dependencies.")
-         transit-writer (try-resolve 'cognitect.transit/writer)
-         transit-read (try-resolve 'cognitect.transit/read)
-         transit-reader (try-resolve 'cognitect.transit/reader)]
+(defn ^:internal ^:no-doc transit-codec
+  [{:keys [name content-type type read-handlers write-handlers]
+    :or {name :transit type :json}}]
+  (let [content-type (or content-type (str "application/transit+"
+                                        (name type)))
+        transit-write (try-resolve-throw `cognitect.transit/write
+                        "add com.cognitect/transit-clj to your dependencies.")
+        transit-writer (try-resolve 'cognitect.transit/writer)
+        transit-read (try-resolve 'cognitect.transit/read)
+        transit-reader (try-resolve 'cognitect.transit/reader)]
     (make-codec
       :name name
       :content-type content-type
@@ -64,12 +52,33 @@
                   (catch Throwable e
                     (throw (decode-error :transit data e))))))))
 
-(set-valid-options! transit-codec #{:name :content-type :type
-                                    :read-handlers :write-handlers})
-
 (defn register-transit-codec
-  "Creates and registers a Transit-based codec in one step.
+  "Creates and registers a codec that can be used to encode to/decode from Transit.
 
-   Takes the same options as {{transit-codec}}."
+   To use vanilla Transit, call this wih no options and use :transit
+   when passing an encoding to functions that take such.
+
+   If you need to provide custom handlers, you can provide them as
+   options. The custom handlers are merged with the default handlers by
+   transit-clj itself.
+
+   options can be a map or kwargs, with these valid keys [default]:
+
+   * :name - the name of the encoding [:transit]
+   * :type - the type of the underlying format. one of: :json, :json-verbose,
+     :msgpack [:json]
+   * :content-type - the content type for the encoding
+     [\"application/transit+<:type>\"]
+   * :write-handlers - the full set of handlers to use for writing. If not
+     provided, the built-in Transit defaults are used [nil]
+   * :read-handlers the full set of handlers to use for reading. If not
+     provided, the built-in Transit defaults are used [nil]"
   [& options]
-  (register-codec (apply transit-codec options)))
+  (-> options
+    kwargs-or-map->raw-map
+    (validate-options register-transit-codec)
+    transit-codec
+    register-codec))
+
+(set-valid-options! register-transit-codec #{:name :content-type :type
+                                             :read-handlers :write-handlers})
