4f16c6ed16f9c695eaa5cb683805e35571d43eca
Toby Crawley <toby@tcrawley.org>
2014-09-18 21:41:22 -0400

    Update example app to 0.2.0.

--- example-app/project.clj
+++ example-app/project.clj
@@ -3,6 +3,6 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.cognitect/transit-clj "0.8.255"]
                  [org.immutant/immutant "2.x.incremental.296"]
-                 [org.immutant/immutant-transit "0.1.0"]]
+                 [org.immutant/immutant-transit "0.2.0"]]
   :repositories [["Immutant incremental builds"
                   "http://downloads.immutant.org/incremental/"]])
